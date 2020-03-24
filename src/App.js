8501c124a15f00ffb0d9526fe309392281b7d7a1
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2020-03-24 17:24:12 -0400

    added state for rate conversions and reached mvp

--- src/App.js
+++ src/App.js
@@ -7,8 +7,23 @@ function App() {
   const [currencyLocations, setCurrencyLocations] = useState([]);
   const [fromLocation, setFromLocation] = useState();
   const [toLocation, setToLocation] = useState();
+  const [exchangeRate, setExchangeRate] = useState();
+  const [amount, setAmount] = useState(1);
+  const [amountInFromLocation, setAmountInFromLocation] = useState(true);
+
+  let toAmount;
+  let fromAmount;
+
+  if (amountInFromLocation) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
 
   console.log(currencyLocations);
+  console.log(exchangeRate);
   useEffect(() => {
     axios.get("https://api.exchangeratesapi.io/latest").then(data => {
       console.log(data);
@@ -16,8 +31,32 @@ function App() {
       setCurrencyLocations([data.data.base, ...Object.keys(data.data.rates)]);
       setFromLocation(data.data.base);
       setToLocation(firstCurrency);
+      setExchangeRate(data.data.rates[firstCurrency]);
     });
   }, []);
+
+  useEffect(() => {
+    if (fromLocation != null && toLocation != null) {
+      axios
+        .get(
+          `https://api.exchangeratesapi.io/latest?base=${fromLocation}&symbols=${toLocation}`
+        )
+        .then(data => {
+          console.log(data.data.rates[toLocation]);
+          setExchangeRate(data.data.rates[toLocation]);
+        });
+    }
+  }, [fromLocation, toLocation]);
+
+  const fromAmountChange = e => {
+    setAmount(e.target.value);
+    setAmountInFromLocation(true);
+  };
+
+  const toAmountChange = e => {
+    setAmount(e.target.value);
+    setAmountInFromLocation(false);
+  };
 
   return (
     <>
@@ -25,11 +64,17 @@ function App() {
       <CurrencyRow
         currencyLocations={currencyLocations}
         selectedLocation={fromLocation}
+        onChangeLocation={e => setFromLocation(e.target.value)}
+        onChangeExchange={fromAmountChange}
+        amount={fromAmount}
       />
       <div className="equals">=</div>
       <CurrencyRow
         currencyLocations={currencyLocations}
         selectedLocation={toLocation}
+        onChangeLocation={e => setToLocation(e.target.value)}
+        onChangeExchange={toAmountChange}
+        amount={toAmount}
       />
     </>
   );
--- src/components/CurrencyRow.js
+++ src/components/CurrencyRow.js
@@ -1,11 +1,17 @@
 import React from "react";
 
-const CurrencyRow = ({ currencyLocations, selectedLocation }) => {
+const CurrencyRow = ({
+  currencyLocations,
+  selectedLocation,
+  onChangeLocation,
+  onChangeExchange,
+  amount
+}) => {
   console.log(currencyLocations);
   return (
     <div>
-      <input type="number" />
-      <select value={selectedLocation}>
+      <input type="number" value={amount} onChange={onChangeExchange} />
+      <select value={selectedLocation} onChange={onChangeLocation}>
         {currencyLocations.map(location => {
           return (
             <option value={location} key={location}>
