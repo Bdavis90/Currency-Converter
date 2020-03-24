0944da5cb3fbc09d294d4617c8ced7c4e9ff858b
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2020-03-24 16:13:38 -0400

    Created inputs for converter

---
+++ src/components/CurrencyRow.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CurrencyRow = () => {
+  return (
+    <div>
+      <input type="number" className="input" />
+      <select>
+        <option>Value</option>
+      </select>
+    </div>
+  );
+};
+
+export default CurrencyRow;
